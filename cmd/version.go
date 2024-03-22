96c99c869944de7aac90bff14d9464159fe3266b
suman chapai <sumanchapai@gmail.com>
2024-03-22 12:18:22 +0545

    feat: add version command

---
+++ cmd/version.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	_ "embed"
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+//go:embed .version
+var version string
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "print ad2bs version",
+	Run: func(_ *cobra.Command, _ []string) {
+		fmt.Printf("%v\n", version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
